0557aed083244f2e0faaef3c39a12af058e7d45a
TorgunI <torgunivan@gmail.com>
2022-04-17 22:41:35 +0300

    Добавил калькулятор Характерстик

--- Haveron/Menu.cs
+++ Haveron/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Haveron.Калькулятор_характеристик;
 
 namespace Haveron
 {
@@ -10,36 +11,37 @@ namespace Haveron
     {
         private PlayersListBuilder _playersListBuilder;
         private PlayerBuilder _playerBuilder;
-
-
+        private CharacteristicCalculator _characteristicCalculator;
 
         public Menu()
         {
             _playersListBuilder = new PlayersListBuilder();
             _playerBuilder = new PlayerBuilder(_playersListBuilder.GetChosenPlayer(0));
+            _characteristicCalculator = new CharacteristicCalculator();
         }
 
-        public void Run()
+        public void RunMenu()
         {
             bool isRun = true;
+
             while (isRun)
             {
-                _playerBuilder.ShowCurrentPlayer();
-
-                Console.WriteLine("[1] - Взаимодействие с выбранным персонажем\n" +
-                    "[2] - Взаимодействие со списком игроков\n" +
-                    "[3] - Выход");
+                Console.WriteLine("Выберите меню, которое хотите запустить:\n" +
+                    "[1] - Калькулятор персонажа\n" +
+                    "[2] - Калькулятор характеристик\n" +
+                    "[3] - Рандомайзер вещей\n" +
+                    "[0] - Выход");
 
                 Console.Write("Ввод: ");
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        RunPlayerMenu();
+                        RunPlayerCalculator();
                         break;
                     case "2":
-                        RunPlayersListMenu();
+                        _characteristicCalculator.ChangeHumanStat();
                         break;
-                    case "3":
+                    case "0":
                         isRun = false;
                         break;
                     default:
@@ -53,12 +55,47 @@ namespace Haveron
             }
         }
 
+        private void RunPlayerCalculator()
+        {
+            bool isRun = true;
+            while (isRun)
+            {
+                _playerBuilder.ShowCurrentPlayer();
+
+                Console.WriteLine("[1] - Взаимодействие с выбранным персонажем\n" +
+                    "[2] - Взаимодействие со списком игроков\n" +
+                    "[0] - Выход");
+
+                Console.Write("Ввод: ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        RunPlayerMenu();
+                        break;
+                    case "2":
+                        RunPlayersListMenu();
+                        break;
+                    case "0":
+                        isRun = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неправильная команда!");
+                        break;
+                }
+
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+
         private void RunPlayerMenu()
         {
             Console.WriteLine("\n[1] - Изменить характеристику\n" +
                 "[2] - Распределить очки уровня\n" +
                 "[3] - Настройка вывода информации об игроке\n" +
-                "[5] - Выход из меню персонажа");
+                "[0] - Выход из меню персонажа");
             Console.Write("Ввод: ");
 
             switch (Console.ReadLine())
@@ -72,7 +109,7 @@ namespace Haveron
                 case "3":
                     _playerBuilder.DisplayOutputSelection();
                     break;
-                case "5":
+                case "0":
                     break;
                 default:
                     Console.WriteLine("Неправильная команда!");
@@ -86,17 +123,16 @@ namespace Haveron
                     "[2] - Создать игрока\n" +
                     "[3] - Вывести список игроков\n" +
                     "[4] - Очистить список персонажей\n" +
-                    "[5] - Выход из меню персонажа");
+                    "[0] - Выход из меню персонажа");
             Console.Write("Ввод: ");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    if(_playersListBuilder.IsPlayerChosen(out int playerIndex))
-                        _playerBuilder = new PlayerBuilder(_playersListBuilder.GetChosenPlayer(playerIndex));
+                    _playerBuilder.ChangePlayer(_playersListBuilder.TryChoosePlayer());
                     break;
                 case "2":
-                    _playersListBuilder.CreationPlayerMenu();
+                    _playerBuilder.ChangePlayer(_playersListBuilder.GetNewPlayer());
                     break;
                 case "3":
                     _playersListBuilder.ShowPlayersList();
@@ -104,7 +140,7 @@ namespace Haveron
                 case "4":
                     _playersListBuilder.ClearPlayersList();
                     break;
-                case "5":
+                case "0":
                     break;
                 default:
                     Console.WriteLine("Неправильная команда!");
